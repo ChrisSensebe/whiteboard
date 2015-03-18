ec748699914f34d2d8dbde10a16f78f1920916af
Christophe Sensebé <christophe.sensebe@gmail.com>
2015-03-18 21:07:06 +0000

    playing with routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nizural' });
+  res.render('index', { title: 'Nizural', article: 'Section accueil' });
+})
+
+/* GET sections pages. */
+.get('/code', function(req, res, next) {
+  res.render('index', { title: 'Nizural', article: 'Section code' });
+})
+.get('/raspberry', function(req, res, next) {
+  res.render('index', { title: 'Nizural', article: 'Section raspberrypi' });
+})
+.get('/photo', function(req, res, next) {
+  res.render('index', { title: 'Nizural', article: 'Section Photo' });
+})
+.get('/about', function(req, res, next) {
+  res.render('index', { title: 'Nizural', article: 'Section présentation' });
 });
 
 module.exports = router;
