53e2c1e0104c61c1e13e6c463095ab0552d418e9
Christophe Sensebé <christophe.sensebe@gmail.com>
2015-03-26 18:00:57 +0000

    creating welcome page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router  = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Nizural'});
+  res.render('index', {title: 'Whiteboard'});
 });
 
 module.exports = router;
