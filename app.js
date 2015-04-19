23a217aea38e6de0476dbd6064acc7b2db7fc456
Christophe Sensebé <christophe.sensebe@gmail.com>
2015-04-19 15:36:11 +0000

    moving router middleware before static middlware

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+// routes
+app.use('/', routes);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
 
@@ -51,9 +53,6 @@ app.use(passport.session());
 
 // initializing password
 initPassword(passport);
-
-// routes
-app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
