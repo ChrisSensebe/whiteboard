fcb5404a5dcd966c209bfd0aff8a798b39e5ac12
Christophe Sensebé <christophe.sensebe@gmail.com>
2015-04-20 19:58:20 +0000

    deleted app.js, adding config/application.js & server.js

---
+++ config/application.js
@@ -0,0 +1,54 @@
+var express        = require('express'),
+    packageJson    = require('../package.json'),
+    path           = require('path'),
+    env            = process.env.NODE_ENV || 'development',
+    routes         = require('../routes/routesIndex'),
+    favicon        = require('serve-favicon'),
+    bodyParser     = require('body-parser'),
+    cookieParser   = require('cookie-parser'),
+    expressSession = require('express-session'),
+    mongoose       = require('mongoose'),
+    flash          = require('connect-flash'),
+    csurf          = require('csurf');
+
+console.log('Loading Whiteboard.');
+
+global.App = {
+    app     : express(),
+    env     : env,
+    port    : process.env.PORT || 8080,
+    version : packageJson.version,
+    root    : path.join(__dirname, '..'),
+    appPath : function(path){return this.root + '/' + path},
+    require : function(path){return require(this.appPath(path))},
+    start   : function(){
+        if(!this.started){
+            this.started = true;
+            this.app.listen(this.port);
+            console.log('Running App version ' + App.version + ' on port ' + App.port + ' in ' + App.env + ' mode.');
+        }
+    }
+};
+
+// database connection
+mongoose.connect(process.env.IP + '/whiteboard');
+
+// view engine setup
+App.app.set('views', App.appPath('views'));
+App.app.set('view engine', 'ejs');
+
+/* Middlewares */ 
+
+// uncomment after placing your favicon in /public
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+App.app.use(bodyParser.json());
+App.app.use(bodyParser.urlencoded({ extended: false }));
+App.app.use(cookieParser());
+App.app.use(expressSession({
+  secret: 'Top secret Trololo',
+  saveUninitialized: true,
+  resave: true}));
+App.app.use(csurf());
+App.app.use(flash());
+App.app.use(express.static(App.appPath('public')));
+App.app.use('/', routes);
