b1c559e9ce8a38f02c45916a049d3c1afe2ac861
Christophe Sensebé <christophe.sensebe@gmail.com>
2015-04-21 10:11:16 +0000

    changing article title & user username to be uniques in db

--- models/article.js
+++ models/article.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var schema   = mongoose.Schema({
-   title       : String,
+   title       : {type : String, unique : true},
    articleBody : String
 });
 
--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var schema   = mongoose.Schema({
-   username : String,
+   username : {type : String, unique : true},
    email    : String,
    password : String
 });
