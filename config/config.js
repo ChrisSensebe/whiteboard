87de8afc211a289069e668c77404b055e2b255c0
Christophe Sensebé <christophe.sensebe@gmail.com>
2015-04-30 19:25:59 +0000

    readding file

---
+++ config/config.js
@@ -0,0 +1,8 @@
+// change this to configure your session secret and database url"
+
+var config = {
+    sessionSecret : 'Your session secret',
+    database      : 'Your database url'
+};
+
+module.exports = config;
