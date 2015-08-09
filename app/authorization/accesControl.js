de28b4c7e7698a3f1df588cf9d144f9e10efda37
Christophe Sensebé <christophe.sensebe@gmail.com>
2015-08-09 18:08:28 +0000

    installing package authorized & creating accesControl.js

---
+++ app/authorization/accesControl.js
@@ -0,0 +1,14 @@
+var auth    = require ('authorized'),
+    Article = require('../models/article');
+
+auth.role('article.author', function (record, req, cb){
+    cb(null, req.user.id === record._id.toString());
+});
+    
+auth.entity('article', function fetchArticle(req, cb){
+    Article.findById(req.params.id, cb);
+});
+
+auth.action('edit article', ['article.author']);
+
+module.exports = auth;
