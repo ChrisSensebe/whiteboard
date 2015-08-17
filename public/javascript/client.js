9328a15d0473baaae73d3ed3324b62e194642102
Christophe Sensebé <christophe.sensebe@gmail.com>
2015-08-17 11:02:34 +0000

    adding password check in signup & profile.ejs

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -3,3 +3,29 @@
 $('#hideFlash').click(function(){
     $('#flash').hide();
 });
+
+
+// Checks password match in forms
+
+$(document).ready(function(){
+    $('#confirmPassword').keyup(checkPasswordMatch);
+});
+
+function checkPasswordMatch(){
+    
+    var password        = $("#password").val();
+    var confirmPassword = $("#confirmPassword").val();
+
+    if (password != confirmPassword){
+        $('#passwordMatch').text('Passwords must match');
+        $('#confirmPassword').css('background-color', '#f2dede');
+        $('#confirmPassword').css('border-color', '#a94442');
+        $('#sendButton').prop('disabled', true);
+    }
+    else{
+        $('#passwordMatch').text('');
+        $('#confirmPassword').css('background-color', '#ffffff');
+        $('#confirmPassword').css('border-color', '#66afe9');
+        $('#sendButton').prop('disabled', false);
+    }
+}
