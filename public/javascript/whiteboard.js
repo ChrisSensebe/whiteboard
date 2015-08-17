b641402f5a194bc935ffe209362a9b57805a66ca
Christophe Sensebé <christophe.sensebe@gmail.com>
2015-08-17 12:30:36 +0000

    adding frond end form validation for login/signup/updateProfile

--- public/javascript/whiteboard.js
+++ public/javascript/whiteboard.js
@@ -5,27 +5,87 @@ $('#hideFlash').click(function(){
 });
 
 
-// Checks password match in forms
+// form validation
 
 $(document).ready(function(){
-    $('#confirmPassword').keyup(checkPasswordMatch);
+    $('#confirmPassword').keyup(passwordCheck);
+    $('#email').keyup(validateEmail);
+    $('#username').keyup(validateUsername);
+    $('#password').keyup(validatePassword);
 });
 
-function checkPasswordMatch(){
+function passwordCheck(){
     
     var password        = $("#password").val();
     var confirmPassword = $("#confirmPassword").val();
-
+    
     if (password != confirmPassword){
-        $('#passwordMatch').text('Passwords must match');
+        $('#confirmPasswordText').text('Passwords must match');
         $('#confirmPassword').css('background-color', '#f2dede');
         $('#confirmPassword').css('border-color', '#a94442');
         $('#sendButton').prop('disabled', true);
     }
     else{
-        $('#passwordMatch').text('');
+        $('#confirmPasswordText').text('');
         $('#confirmPassword').css('background-color', '#ffffff');
         $('#confirmPassword').css('border-color', '#66afe9');
+        $('#sendButton').prop('disabled', false);
+    }
+}
+
+function validateEmail(){
+    
+    var emailReg = /^[\S]+@[\S]+\.[\S]+$/;
+    var email    = $('#email').val();
+    
+    if(!emailReg.test(email)){
+        $('#emailText').text('Must be a valid email adress');
+        $('#email').css('background-color', '#f2dede');
+        $('#email').css('border-color', '#a94442');
+        $('#sendButton').prop('disabled', true);
+    }
+    else{
+        $('#emailText').text('');
+        $('#email').css('background-color', '#ffffff');
+        $('#email').css('border-color', '#66afe9');
+        $('#sendButton').prop('disabled', false);
+    }
+}
+
+function validateUsername(){
+    
+    var usernameReg = /^[a-zA-Z0-9_-]{3,20}$/;
+    var username    = $('#username').val();
+    
+    if(!usernameReg.test(username)){
+        $('#usernameText').text('Username must be between 3 and 20 caracters, and must contain only letters, numbers, - and _ symbols');
+        $('#username').css('background-color', '#f2dede');
+        $('#username').css('border-color', '#a94442');
+        $('#sendButton').prop('disabled', true);
+    }
+    else{
+        $('#usernameText').text('');
+        $('#username').css('background-color', '#ffffff');
+        $('#username').css('border-color', '#66afe9');
+        $('#sendButton').prop('disabled', false);
+    }
+}
+
+function validatePassword(){
+    
+    var passwordReg = /^.{3,20}$/;
+    var password    = $("#password").val();
+    
+    if(!passwordReg.test(password)){
+        $('#passwordText').text('Password must be between 3 and 20 caracters');
+        $('#password').css('background-color', '#f2dede');
+        $('#password').css('border-color', '#a94442');
+        $('#sendButton').prop('disabled', true);
+    }
+    else{
+        $('#passwordText').text('');
+        $('#password').css('background-color', '#ffffff');
+        $('#password').css('border-color', '#66afe9');
         $('#sendButton').prop('disabled', false);
     }
 }
